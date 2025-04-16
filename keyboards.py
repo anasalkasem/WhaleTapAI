b74b458c837f5a40fc14bdeec4c072daaffc85d0
anasalkasem <anass111173@gmail.com>
2025-04-16 10:23:09 -0500

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,7 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def plans_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("⭐ اشتراك PRO - 10 USDT/شهر", callback_data="pay_pro_usdt")],
-        [InlineKeyboardButton("💎 اشتراك PRO - 10 SOL/شهر", callback_data="pay_pro_sol")]
-    ])
