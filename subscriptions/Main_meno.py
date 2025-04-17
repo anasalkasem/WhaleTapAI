7da17accdc4efaba9b91f57b470d910889f68eb1
anasalkasem <anass111173@gmail.com>
2025-04-17 15:38:45 -0500

    Update Main_meno.py

--- subscriptions/Main_meno.py
+++ subscriptions/Main_meno.py
@@ -1,13 +1,16 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def main_menu_keyboard():
     keyboard = [
-        [InlineKeyboardButton("شراء", callback_data="buy"), InlineKeyboardButton("بيع", callback_data="sell")],
-        [InlineKeyboardButton("نسخ التداول", callback_data="copy_trade"), InlineKeyboardButton("التداول التلقائي", callback_data="auto_trade")],
-        [InlineKeyboardButton("أوامر الحد", callback_data="limit_orders")],
-        [InlineKeyboardButton("العملات لدي", callback_data="my_tokens"), InlineKeyboardButton("المحفظة", callback_data="wallet")],
-        [InlineKeyboardButton("المساعدة", callback_data="help")],
-        [InlineKeyboardButton("المحفظة الذكية", callback_data="smart_wallet"), InlineKeyboardButton("امتداد", callback_data="extension")],
-        [InlineKeyboardButton("الإعدادات", callback_data="settings"), InlineKeyboardButton("الإحالات", callback_data="referrals")]
+        [InlineKeyboardButton("شراء", callback_data="buy")],
+        [InlineKeyboardButton("بيع", callback_data="sell")],
+        [InlineKeyboardButton("نسخ التداول", callback_data="copy_trade")],
+        [InlineKeyboardButton("التداول التلقائي", callback_data="auto_trade")],
+        [InlineKeyboardButton("إيقاف النسخ", callback_data="stop_copy")],
+        [InlineKeyboardButton("المحفظة", callback_data="wallet")],
+        [InlineKeyboardButton("إنشاء محفظة", callback_data="create_wallet")],
+        [InlineKeyboardButton("تنبيهات", callback_data="alerts")],
+        [InlineKeyboardButton("حد الخسارة", callback_data="stop_loss")],
+        [InlineKeyboardButton("تغيير اللغة", callback_data="change_language")]
     ]
     return InlineKeyboardMarkup(keyboard)
