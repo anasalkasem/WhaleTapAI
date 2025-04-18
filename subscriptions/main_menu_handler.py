56ae1006401ce5748ca41436e23a0f7a4951c9e7
anasalkasem <anass111173@gmail.com>
2025-04-17 23:55:56 -0500

    Update main_menu_handler.py

--- subscriptions/main_menu_handler.py
+++ subscriptions/main_menu_handler.py
@@ -1,18 +1,12 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from subscriptions.main_menu import main_menu_keyboard
+from .main_menu import main_menu_keyboard
 
 async def handle_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message:
-        await update.message.reply_text(
-            "🚀 Welcome to WhaleTap!
-Choose an option below:",
-            reply_markup=main_menu_keyboard()
-        )
-    elif update.callback_query:
-        await update.callback_query.answer()
-        await update.callback_query.edit_message_text(
-            "🚀 Welcome to WhaleTap!
-Choose an option below:",
-            reply_markup=main_menu_keyboard()
-        )
+    query = update.callback_query
+    await query.answer()
+
+    await query.edit_message_text(
+        "🚀 Welcome to WhaleTap!\n\nChoose an option below:",
+        reply_markup=main_menu_keyboard()
+    )
