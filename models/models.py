c564a1000b37bb21f57b64a69fd0a2d0b2a48b9b
anasalkasem <anass111173@gmail.com>
2025-04-21 09:45:19 -0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,1 +1,10 @@
+from sqlalchemy import Column, BigInteger, String, DateTime
+from models.database import Base
+import datetime
 
+class Subscription(Base):
+    __tablename__ = 'subscriptions'
+
+    user_id = Column(BigInteger, primary_key=True)
+    plan_type = Column(String, nullable=False)  # 'free' أو 'pro'
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
