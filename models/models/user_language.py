a770db04151ebba1f92b3cce61e6f681dd7c6d1a
anasalkasem <anass111173@gmail.com>
2025-04-25 15:51:15 -0500

    Delete models/models directory

--- models/models/user_language.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from models.database import Base
-
-class UserLanguage(Base):
-    __tablename__ = "user_languages"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, unique=True, nullable=False)
-    language = Column(String, default="en")
