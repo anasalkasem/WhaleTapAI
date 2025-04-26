2632365032b1217e862b1c3bf9609ff490350c2f
anasalkasem <anass111173@gmail.com>
2025-04-26 10:18:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# main.py
+
 import os
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 
@@ -10,13 +12,13 @@ from subscriptions.stop_copying_handler import handle_stop_copying
 from subscriptions.stats_handler import handle_my_stats
 from subscriptions.free_plan_handler import handle_free_plan
 from admin.confirm_payment_handler import handle_confirm_payment
-from models.init_db import init_db
 from subscriptions.trading_menu_handler import handle_trading_menu
+
+from models.init_db import init_db
 
 # Initialize the database
 init_db()
 
-# Initialize the bot application
 TOKEN = os.getenv("BOT_TOKEN")
 application = Application.builder().token(TOKEN).build()
 
@@ -27,7 +29,7 @@ application.add_handler(CommandHandler("start", handle_main_menu))
 application.add_handler(CallbackQueryHandler(handle_main_menu, pattern="^main_menu$"))
 application.add_handler(CallbackQueryHandler(handle_subscription_info, pattern="^subscription_info$"))
 application.add_handler(CallbackQueryHandler(handle_settings, pattern="^settings$"))
-application.add_handler(CallbackQueryHandler(handle_auto_trading, pattern="^auto_trading$"))
+application.add_handler(CallbackQueryHandler(handle_auto_trade_setting, pattern="^auto_trading$"))
 application.add_handler(CallbackQueryHandler(handle_copy_trade, pattern="^copy_trade$"))
 application.add_handler(CallbackQueryHandler(handle_smart_insights, pattern="^smart_insights$"))
 application.add_handler(CallbackQueryHandler(handle_stop_copying, pattern="^stop_copying$"))
@@ -36,6 +38,5 @@ application.add_handler(CallbackQueryHandler(handle_free_plan, pattern="^subscri
 application.add_handler(CallbackQueryHandler(handle_confirm_payment, pattern="^admin_confirm_payment$"))
 application.add_handler(CallbackQueryHandler(handle_trading_menu, pattern="^trading$"))
 
-# Run the bot
 if __name__ == "__main__":
     application.run_polling()
