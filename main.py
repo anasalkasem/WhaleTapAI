2838807c44bc723848dbc8f534e8d5f5eb2738cd
anasalkasem <anass111173@gmail.com>
2025-04-25 21:39:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler
-from subscriptions.main_menu_handler import handle_main_menu
+
+# Handlers
+from subscriptions.main_menu_handler import (
+    handle_main_menu,
+    handle_subscription_info,
+    handle_back_to_plans
+)
 from subscriptions.settings_handler import handle_settings
 from subscriptions.auto_trading_handlers import handle_auto_trading
 from subscriptions.copy_trade_handler import handle_copy_trade
@@ -9,16 +15,24 @@ from subscriptions.stop_copying_handler import handle_stop_copying
 from subscriptions.stats_handler import handle_my_stats
 from subscriptions.free_plan_handler import handle_free_plan
 from admin.confirm_payment_handler import handle_confirm_payment
+
+# Database init
 from models.init_db import init_db
 
+# Initialize DB
 init_db()
 
+# Bot setup
 TOKEN = os.getenv("BOT_TOKEN")
 application = Application.builder().token(TOKEN).build()
 
+# Start command
 application.add_handler(CommandHandler("start", handle_main_menu))
 
+# Callback query handlers
 application.add_handler(CallbackQueryHandler(handle_main_menu, pattern="^main_menu$"))
+application.add_handler(CallbackQueryHandler(handle_subscription_info, pattern="^subscription_info$"))
+application.add_handler(CallbackQueryHandler(handle_back_to_plans, pattern="^back_to_plans$"))
 application.add_handler(CallbackQueryHandler(handle_settings, pattern="^settings$"))
 application.add_handler(CallbackQueryHandler(handle_auto_trading, pattern="^auto_trading$"))
 application.add_handler(CallbackQueryHandler(handle_copy_trade, pattern="^copy_trade$"))
