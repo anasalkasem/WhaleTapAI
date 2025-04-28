6c2e543a89738c73b8dd0674630163a8ab53ca32
anasalkasem <anass111173@gmail.com>
2025-04-28 01:26:54 -0500

    Update handlers_register.py

--- subscriptions/handlers_register.py
+++ subscriptions/handlers_register.py
@@ -7,7 +7,7 @@ from subscriptions.insights_handler import handle_smart_insights
 from subscriptions.stop_copying_handler import handle_stop_copying
 from subscriptions.stats_handler import handle_my_stats
 from subscriptions.free_plan_handler import handle_free_plan
-from subscriptions.subscription_handler import handle_subscribe_pro, handle_main_menu, handle_subscription_info
+from subscriptions.subscription_handler import handle_main_menu, handle_subscription_info
 from subscriptions.settings_handler import handle_settings
 from admin.confirm_payment_handler import handle_confirm_payment
 
@@ -21,6 +21,5 @@ def add_handlers(application):
     application.add_handler(CallbackQueryHandler(handle_stop_copying, pattern="^stop_copying$"))
     application.add_handler(CallbackQueryHandler(handle_my_stats, pattern="^my_stats$"))
     application.add_handler(CallbackQueryHandler(handle_free_plan, pattern="^subscribe_free$"))
-    application.add_handler(CallbackQueryHandler(handle_subscribe_pro, pattern="^subscribe_pro$"))
     application.add_handler(CallbackQueryHandler(handle_confirm_payment, pattern="^admin_confirm_payment$"))
     application.add_handler(CallbackQueryHandler(handle_trading_menu, pattern="^trading$"))
