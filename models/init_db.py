16f704d27116688dc63fe1ad8e5a63790114557c
anasalkasem <anass111173@gmail.com>
2025-04-28 09:06:49 -0500

    Update init_db.py

--- models/init_db.py
+++ models/init_db.py
@@ -1,15 +1,19 @@
-# models/init_db.py
-
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
 
-# إعداد قاعدة البيانات
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
-metadata = MetaData()
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def init_db():
-    # إنشاء الجداول (لو في موديلات لاحقًا)
-    Base.metadata.create_all(engine)
+    from models.models import Base  # استدعاء الجداول
+    Base.metadata.create_all(bind=engine)
