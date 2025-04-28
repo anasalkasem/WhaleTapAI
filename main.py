905da1101ebfd7b9156727699e556e5e3f1c5260
anasalkasem <anass111173@gmail.com>
2025-04-27 23:57:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from telegram.ext import Application, CommandHandler
-from subscriptions.subscription_handler import add_handlers
 from models.init_db import init_db
+from subscriptions.subscription_handler import add_handlers, handle_main_menu
 
 # تهيئة قاعدة البيانات
 init_db()
@@ -10,15 +10,14 @@ init_db()
 TOKEN = os.getenv("BOT_TOKEN")
 application = Application.builder().token(TOKEN).build()
 
-# أولاً عرف الدالة
+# تعريف دالة /start
 async def handle_start(update, context):
-    from subscriptions.subscription_handler import handle_main_menu
     await handle_main_menu(update, context)
 
-# بعدين استدعيها
+# إضافة هندلر أمر /start
 application.add_handler(CommandHandler("start", handle_start))
 
-# إضافة كل الهاندلرات مرة وحدة
+# إضافة جميع الهاندلرات الأخرى
 add_handlers(application)
 
 if __name__ == "__main__":
